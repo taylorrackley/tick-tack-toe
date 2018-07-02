736bd7939440e9f7c916e16fd06ef9bc6aa7bd3f
Taylor Rackley <taylorrack@sbcglobal.net>
2018-07-01 21:05:28 -0400

    player join new game on opponent disconnect

--- server.js
+++ server.js
@@ -9,28 +9,38 @@ app.use(express.static(path));
 queue = [];
 names = {};
 rooms = {};
+socketRoom = {};
 symbols = {};
 
 var findOpponent = function(socket) {
     if(queue.length > 0) {
-        var oppenent = queue.pop();
+        var opponent = queue.pop();
         var blocks = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        var room_id = socket.id+'#'+oppenent.id;
-        var room = {'blocks' : blocks, 'active_player' : oppenent.id};
+        var room_id = socket.id+'#'+opponent.id;
+        var room = {'blocks' : blocks, 'active_player' : opponent.id};
 
-        oppenent.join(room_id);
+        opponent.join(room_id);
         socket.join(room_id);
 
-        rooms[room_id] = room;
+        // console.log("opponent id: "+opponent.id);
+        // console.log("socket id: "+socket.id);
+        console.log("room id: "+room_id);
+
+        socketRoom[socket.id] = room_id;
+        socketRoom[opponent.id] = room_id;
+
         rooms[room_id] = room;
 
-        symbols[oppenent.id] = 1;
+        symbols[opponent.id] = 1;
         symbols[socket.id] = 2;
 
-        //io.emit('load');
+        io.emit('load');
 
-        oppenent.emit('game-join', {'name' : names[socket.id], 'room_id' : room_id});
-        socket.emit('game-join', {'name' : names[oppenent.id], 'room_id' : room_id});
+        opponent.emit('game-join', {'name' : names[socket.id], 'room_id' : room_id});
+        socket.emit('game-join', {'name' : names[opponent.id], 'room_id' : room_id});
+
+        opponent.emit('player-turn', opponent.id);
+        socket.emit('player-turn', opponent.id);
 
     } else {
         queue.push(socket);
@@ -65,16 +75,16 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     socket.on('login', function(data) {
-        console.log("Username: "+data.username+" : Socket: "+socket.id);
+        console.log("Username: "+data.username+" : Socket ID: "+socket.id);
 
         names[socket.id] = data.username;
-        io.to(socket.id).emit('load');
+        //io.to(socket.id).emit('load');
 
         findOpponent(socket);
     });
     socket.on('box-click', function(data) {
-        console.log("Box has been clicked : "+data.box_id);
-        var tempRoom = rooms[data.room_id];
+        //console.log("Box has been clicked : "+data.box_id);
+        var tempRoom = rooms[socketRoom[socket.id]];
         // If it's the player's turn
         if(socket.id == tempRoom.active_player) {
             // If box has not been clicked yet
@@ -82,7 +92,10 @@ io.on('connection', function(socket) {
                 // Set box selection to the active player
                 tempRoom.blocks[data.box_id-1] = symbols[socket.id];
                 // Change active player
-                rooms[data.room_id].active_player = data.room_id.replace(socket.id, '').replace('#', '');
+                var opponentID = data.room_id.replace(socket.id, '').replace('#', '');
+                rooms[data.room_id].active_player = opponentID;
+                socket.emit('player-turn', opponentID);
+                io.to(opponentID).emit('player-turn', opponentID);
                 // Update box in clients
                 io.to(data.room_id).emit('box-click', {'box_id' : data.box_id, 'symbol' : symbols[socket.id]});
                 // Check for win
@@ -91,6 +104,19 @@ io.on('connection', function(socket) {
                     io.to(data.room_id).emit('game-win', {'winner' : socket.id });
                 }
             }
+        }
+
+    });
+    socket.on('disconnect', function() {
+
+        var roomID = socketRoom[socket.id];
+        if(roomID != null) {
+            console.log("socketID: "+socket.id);
+            console.log("disconnect roomID: "+roomID);
+            var opponentID = roomID.replace(socket.id, '').replace('#', '');
+
+            socketRoom[opponentID] = null;
+            findOpponent(io.sockets.connected[opponentID]);
         }
 
     });
